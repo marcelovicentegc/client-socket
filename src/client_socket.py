528eff1d00731148ad0e1362395c1bd9a30104db
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-09-15 00:05:33 -0300

    fix: client socket nomenclature

--- src/client_socket.py
+++ src/client_socket.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 # AF_INET stands to Address Family Internet (IPv4), and SOCK_STREAM corresponds to TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if (len(sys.argv) != 3):
     print("Please provide a hostname and a port")
@@ -12,8 +12,8 @@ hostname = sys.argv[1]
 port = int(sys.argv[2])
 request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % hostname
 
-s.connect((hostname, port))
-s.send(request.encode())
+client_socket.connect((hostname, port))
+client_socket.send(request.encode())
 
-response = s.recv(1024)
+response = client_socket.recv(1024)
 print(response.decode("utf-8"))
