0dc64870087f309c0f632df9a7e7c5f355974945
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-09-17 21:38:43 -0300

    WIP: lua client socket

---
+++ src/client_socket.lua
@@ -0,0 +1,22 @@
+
+if (#arg ~= 2) then
+    print("please provide a hostname and a socket")   
+end
+
+local host, port = arg[1], arg[2]
+local address = arg[1] .. ":" .. arg[2]
+local socket = require("socket")
+local tcp = assert(socket.tcp())
+
+
+
+tcp:connect(host, port);
+tcp:send("GET / HTTP/1.1\r\nHost:" ..address.. "\r\n\r\n");
+
+while true do
+    local s, status, partial = tcp:receive()
+    print(s or partial)
+    if status == "closed" then break end
+end
+
+tcp:close()
